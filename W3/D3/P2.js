5f484973b71913e214977231ae960cd4caeda12d
incharadn433 <incharadn433@gmail.com>
2026-03-04 14:40:23 +0530

    While loop and do while loop

---
+++ W3/D3/P2.js
@@ -0,0 +1,14 @@
+// While loop
+console.log("While loop examples");
+
+let count = 0;
+while(count<5){
+    console.log("count",count);
+    count++;
+}
+//do while
+let value = 10;
+do{
+    console.log("value:",value);
+    value++;
+}while(value<3);
