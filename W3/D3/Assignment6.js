2f0bb28372bc74570baa628790be6e877f0c1b4e
incharadn433 <incharadn433@gmail.com>
2026-03-09 21:46:11 +0530

    Assignment

---
+++ W3/D3/Assignment6.js
@@ -0,0 +1,50 @@
+function auditReport(reportJSON) {
+
+    const report = JSON.parse(reportJSON);
+
+    let okCount = 0;
+    let failCount = 0;
+
+    
+    for (let key in report.modules) {
+
+        if (report.modules[key] === "OK") {
+            okCount++;
+        }
+
+        if (report.modules[key] === "FAIL") {
+            failCount++;
+            break; 
+        }
+    }
+
+    const summary = {
+        app: report.app,
+        status: report.status,
+        okCount: okCount,
+        failCount: failCount
+    };
+
+    const summaryJSON = JSON.stringify(summary);
+
+    return {
+        summaryObject: summary,
+        summaryJSON: summaryJSON
+    };
+}
+
+
+const reportJSON = `{
+  "app": "Portal",
+  "status": "OK",
+  "modules": {
+    "auth": "OK",
+    "payment": "OK",
+    "results": "FAIL",
+    "profile": "OK"
+  }
+}`;
+
+const result = auditReport(reportJSON);
+
+console.log(result);
