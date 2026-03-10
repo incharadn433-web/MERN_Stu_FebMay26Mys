75db05779bf03751fd73561fb31cf2b55cbb9825
incharadn433 <incharadn433@gmail.com>
2026-03-10 08:56:19 +0530

    try catch basics

---
+++ W4/D1/P1.js
@@ -0,0 +1,28 @@
+// try catch basics
+const error = document.getElementById("error");
+try{
+    console.log("Trying to access undefined variable");
+    console.log(notDefined);
+}
+catch(err){
+    console.log("Error caught",err.name,"-",err.message); 
+    error.innerHTML ="Trying to access undefined variable"
+}
+console.log("Program execution continues");
+
+//JSON parsing error
+let jsonText = "{json}";
+try{
+    let data = JSON.parse(jsonText);
+    console.log(data);
+}
+catch(err){
+    console.log("JSON parse error: ",err.message);
+}
+try{
+    let num = 10;
+    num();
+}
+catch(err){
+    console.log("Caught error: ",err.name);
+}
