bbc5852f14e8c4aa8276aee846fc9b9ddb5c21ee
incharadn433 <incharadn433@gmail.com>
2026-03-25 09:52:52 +0530

    Introduction to NodeJS File System (fs) built-in module

---
+++ W6/D3/P1.js
@@ -0,0 +1,10 @@
+//Introduction to NodeJS File System (fs) built-in module
+
+const fs = require("fs");
+const fsPromises = require("fs/promises");
+
+console.log("Type of fs.readFile: ",typeof fs.readFile);
+console.log("Type of fs.writeFile: ",typeof fs.writeFile);
+
+console.log("Type of fsPromises.readFile: ",typeof fs.readFile);
+console.log("Type of fsPromises.writeFile: ",typeof fs.writeFile);
