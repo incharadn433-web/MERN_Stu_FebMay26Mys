c9d560bc234d27a3f4142c4a44efb6cc11d08338
incharadn433 <incharadn433@gmail.com>
2026-03-30 12:12:47 +0530

    Middleware usage in Express

--- W7/D1/P4.js
+++ W7/D1/P4.js
@@ -8,3 +8,16 @@ const express = require("express");
 const app = express();
 //app.use registers middleware.
 //This middleware runs for every incoming request.
+app.use(function(req,res,next){
+    console.log("Request received",req.method,res.url);
+    next();
+    //next() is required when this middleware does not finish
+    //the response
+});
+app.get("/",function(req,res){
+    res.send("Middleware executed before the route.");
+});
+app.listen(3000,function(){
+    console.log("Express server running at http://localhost:3000");
+    
+});
