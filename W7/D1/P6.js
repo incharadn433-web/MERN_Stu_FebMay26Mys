f9db059fe5dedb08d781ece8a803f2cb865783af
incharadn433 <incharadn433@gmail.com>
2026-03-31 15:11:21 +0530

    Sending JSON response in express

---
+++ W7/D1/P6.js
@@ -0,0 +1,25 @@
+//Sending JSON response in express
+const express = require("express");
+
+const app = express();
+
+app.get("/api/status",function(req,res){
+    //res.JSON() automatically serializes the object and
+    //sets the JSON content type
+    res.json({
+        success:true,
+        message:"Express API is working"
+    });
+});
+app.get("/api/error",function(req,res){
+    //res.JSON() automatically serializes the object and
+    //sets the JSON content type
+    res.status(404).json({
+        success:false,
+        message:"Requested resource is not found"
+    });
+});
+app.listen(3000,function(){
+    console.log("Express server running at http://localhost:3000");
+    
+});
