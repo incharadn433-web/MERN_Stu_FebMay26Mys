d12f2892342d948e6eacbdc59e4ddb04a0fc2022
incharadn433 <incharadn433@gmail.com>
2026-04-08 10:30:03 +0530

    Middleware for req log

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/middleware/loggerMiddleware.js
@@ -0,0 +1,8 @@
+//Middleware for req log
+function loggerMiddleware(req,res,next){
+    console.log(`${req.metod} ${req.originalUrl}`);
+    next();
+}
+
+module.exports = loggerMiddleware;
+//Task:write the log to a file along with timestamp with both & res
