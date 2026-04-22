d4370fe49ff81bee0c8cf715be3d92d52683eeca
incharadn433 <incharadn433@gmail.com>
2026-04-22 14:25:57 +0530

    service function related to OTP generation created

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/services/auth.service.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/services/auth.service.js
@@ -4,46 +4,48 @@ const otpService = require("./otp.service");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-//Register user 
-exports.registerUser = async ({ name, email, password }) => {
-    const exitingUser = await User.findOne({ email });
+// Register user
+exports.registerUser = async ({name,email,password}) =>{
+    const existingUser = await User.findOne({email});
 
-    if (exitingUser) {
+    if (existingUser) {
         throw new Error("User already exists");
     }
 
     const user = await User.create({
         name,
         email,
-        password
+        password,
     });
 
     await otpService.generateOTP(email);
 
-    return { email: user.email };
+    return {email:user.email};
 };
 
-//Verify OTP 
-exports.verifyOTP = async ({ email, otp }) => {
-    const record = await OTP.findOne({ email }).select("+otp");
+//Verify OTP
+exports.verifyOTP = async({email,otp})=>{
+    const record = await OTP.findOne({email}).select("+otp");
 
     if (!record) {
         throw new Error("OTP expired or not found");
     }
 
-    const isMatch = await bcrypt.compare(otp, record.otp);
+    const isMatch = await bcrypt.compare(otp,record.otp);
 
-    if (!match) {
-        record.attempts += 1;
+    if (!isMatch) {
+        record.attempts +=1;
         await record.save();
         throw new Error("Invalid OTP");
     }
-    await User.updateOne({ email }, { isVerified })
+
+    await User.updateOne({email},{isVerified:true});
+    return true;
 };
 
-//Login
-exports.loginUser = async ({ email, password }) => {
-    const user = await User.findOne({ email }).select("+password");
+// Login
+exports.loginUser = async ({email,password}) =>{
+    const user = await User.findOne({email}).select("+password");
 
     if (!user) {
         throw new Error("User not found");
@@ -56,16 +58,18 @@ exports.loginUser = async ({ email, password }) => {
     if (!isMatch) {
         throw new Error("Invalid credentials");
     }
-    const token = jwt.sign(
-       {id: user._id, role: user.role},
-        process.env.JWT_SECRET,
-        { expiresIn: "1d" });
 
-        return{
-            token,
-            user:{
-                id:user._id,
-                role:user.role,
-            }
-        }
-}
+    const token = jwt.sign(
+        {id:user._id,role:user.role},
+        process.env.JWT_SECRET,
+        {expiresIn:"1d"}
+    );
+
+    return{
+        token,
+        user:{
+            id:user._id,
+            role:user.role,
+        },
+    };
+};
