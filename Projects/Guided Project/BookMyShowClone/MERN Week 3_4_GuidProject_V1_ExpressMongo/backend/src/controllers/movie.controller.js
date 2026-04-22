b2be241ba97abe1b7a27b9a65c950137053fdf8f
incharadn433 <incharadn433@gmail.com>
2026-04-22 14:51:55 +0530

    Created controllers for Movie CRUD operation

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/controllers/movie.controller.js
@@ -0,0 +1,64 @@
+const movieService = require("../services/movie.service");
+
+//Create Movie
+exports.createMovie = async(req,res,next)=>{
+    try{
+        const movie = await movieService.createMovie(req.body);
+
+        res.status(201).json({
+            success:true,
+            message:"Movie created successfuly",
+            data:movie
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
+//Get Movies
+exports.getMovies = async(req,res,next)=>{
+    try{
+        const result = await movieService.getMovies(req.query);
+
+        res.status(200).json({
+            success:true,
+            message:"Movie List fetched",
+            data:result
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
+
+exports.updateMovie = async(req,res,next)=>{
+    try{
+        const movie = await movieService.updateMovie(
+            req.params.id,
+            req.body
+        );
+
+        res.status(200).json({
+            success:true,
+            message:"Movie updated successfuly",
+            data:movie
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
+
+exports.deleteMovie = async(req,res,next)=>{
+    try{
+        await movieService.deleteMovie(req.params.id);
+
+        res.status(200).json({
+            success:true,
+            message:"Movie deleted successfuly"
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
