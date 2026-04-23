9534ca53b29dcf38b680c93b508f12e1cb4ea1f2
incharadn433 <incharadn433@gmail.com>
2026-04-23 13:00:05 +0530

    error forwarding method changed

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Booking.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Booking.js
@@ -36,17 +36,19 @@ const bookingSchema = new mongoose.Schema({
 });
 
 //Add validation
-bookingSchema.pre("save",function(next){
+bookingSchema.pre("save",function(){
     if(this.seats.length === 0){
-        return next(new Error("At least one seat must be selected."))
+        // return next(new Error("At least one seat must be selected."))
+         throw new Error("At least one seat must be selected");
     }
     if(this.totalSeats!=this.seats.length){
-        return next(new Error("Seat count ismatch"));
+        // return next(new Error("Seat count ismatch"));
+          throw new Error("Seat count ismatch");
     }
-    next();
+    
 });
 
 //compound index
 bookingSchema.index({userId:1,showId:1});
 
-mosule.exports = mongoose.model("Booking",bookingSchema);
+module.exports = mongoose.model("Booking",bookingSchema);
