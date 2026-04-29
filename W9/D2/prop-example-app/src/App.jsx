4cf44f3f9e7710efeffc89c7a98b7ffeaf36cde2
incharadn433 <incharadn433@gmail.com>
2026-04-29 09:53:58 +0530

    Container/ wrapper component

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import { PropBasics } from './components/P1'
 import { PropDestructuring } from './components/P2'
+import { PropsChildren } from './components/P3'
 
 function App() {
   return (
     <>
       {/* <PropBasics/> */}
-      <PropDestructuring/>
+      {/* <PropDestructuring/> */}
+      <PropsChildren/>
     </>
   )
 }
