1a28e223b4067024a16ae4286d30e5cbb00a6240
incharadn433 <incharadn433@gmail.com>
2026-05-07 09:59:26 +0530

    Creating the Basics Functions

---
+++ W10/D3/my-event-loop/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { EventBasics } from './components/P1';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <EventBasics />
+    </>
+  )
+}
+
+export default App
