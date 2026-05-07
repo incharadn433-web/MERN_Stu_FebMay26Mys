8aca5c03c72803b7e494ee9a4ef3612c95e46830
incharadn433 <incharadn433@gmail.com>
2026-05-07 10:31:04 +0530

    React Event Object

--- W10/D3/my-event-loop/src/App.jsx
+++ W10/D3/my-event-loop/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
-import { EventBasics } from './components/P1';
+import { EventBasics } from './components/P1'
+import { EventObject } from './components/P2'
 import './App.css'
 
 function App() {
   return (
     <>
-      <EventBasics />
+      {/* <EventBasics /> */}
+      <EventObject />
     </>
   )
 }
