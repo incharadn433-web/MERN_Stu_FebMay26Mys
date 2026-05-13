af837a05f6d6cd0a2f01cdbe277089d0c579f4f1
incharadn433 <incharadn433@gmail.com>
2026-05-13 14:47:03 +0530

    useEffect Lifecycle

--- W11/D3/my-app/src/App.jsx
+++ W11/D3/my-app/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import {ManagingApiState} from "./components/P1";
+import {UseEffectLifecycle} from "./components/P2";
 import './App.css'
 
 function App() {
   return (
     <>
-      < ManagingApiState/>
+      {/* < ManagingApiState/> */}
+      <UseEffectLifecycle/>
     </>
   )
 }
--- W11/D3/my-app/src/components/P2.jsx
+++ W11/D3/my-app/src/components/P2.jsx
@@ -1,10 +1,45 @@
 import { useEffect, useState } from "react";
 
 //useEffect Lifecycle
+//No array:every render
+//[] -> once
+//[state] -> on mount + when state changes
 export function UseEffectLifecycle(){
     const [count,setCount] = useState(0);
 
     useEffect(()=>{
         console.log("Effect 1: runs after every render");
     });
+
+    //Empty dependency array
+    //This runs only once after the component mounts.
+    useEffect(()=>{
+        console.log("Effect 2: runs  only once after mount");
+    },[]);
+
+    //3.Dependency based effect
+    useEffect(()=>{
+        console.log("Effect 3: count changed to",count);
+
+        return()=>{
+            console.log("cleanup for effect 3: previous count:",count);
+        };
+    },[count]);
+
+    //Event handler 
+    function handleIncrement(){
+        setCount((prevCount)=>prevCount+1);
+    }
+    function handleReset(){
+        setCount(0);
+    }
+    return(
+        <section>
+            <h2>useEffect lifecycle</h2>
+            <p>Count: {count}</p>
+            <button onClick={handleIncrement}>Increment</button>
+            <button onClick={handleReset}>Reset</button>
+        </section>
+    )
+
 }
